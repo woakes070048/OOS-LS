825ca5c855d657e51f3221d80ff97efaeedc74b2
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2016-02-25 02:43:21 +0700

    users: invite add source

---
+++ protected/modules/users/views/o/invite/admin_delete.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * User Invites (user-invites)
+ * @var $this InviteController
+ * @var $model UserInvites
+ * @var $form CActiveForm
+ *
+ * @author Putra Sudaryanto <putra.sudaryanto@gmail.com>
+ * @copyright Copyright (c) 2012 Ommu Platform (ommu.co)
+ * @link https://github.com/oMMu/Ommu-Users
+ * @contact (+62)856-299-4114
+ *
+ */
+
+	$this->breadcrumbs=array(
+		'User Invites'=>array('manage'),
+		'Delete',
+	);
+?>
+
+<?php $form=$this->beginWidget('application.components.system.OActiveForm', array(
+	'id'=>'user-invites-form',
+	'enableAjaxValidation'=>true,
+	//'htmlOptions' => array('enctype' => 'multipart/form-data')
+)); ?>
+
+	<div class="dialog-content">
+		<?php echo Phrase::trans(172,0);?>
+		
+	</div>
+	<div class="dialog-submit">
+		<?php echo CHtml::submitButton(Phrase::trans(173,0), array('onclick' => 'setEnableSave()')); ?>
+		<?php echo CHtml::button(Phrase::trans(174,0), array('id'=>'closed')); ?>
+	</div>
+	
+<?php $this->endWidget(); ?>
---
+++ protected/modules/users/views/o/invite/admin_queue_delete.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * User Invite Queue (user-invite-queue)
+ * @var $this InviteController
+ * @var $model UserInviteQueue
+ * @var $form CActiveForm
+ *
+ * @author Putra Sudaryanto <putra.sudaryanto@gmail.com>
+ * @copyright Copyright (c) 2012 Ommu Platform (ommu.co)
+ * @link https://github.com/oMMu/Ommu-Users
+ * @contact (+62)856-299-4114
+ *
+ */
+
+	$this->breadcrumbs=array(
+		'User Invite Queues'=>array('manage'),
+		'Delete',
+	);
+?>
+
+<?php $form=$this->beginWidget('application.components.system.OActiveForm', array(
+	'id'=>'user-invite-queue-form',
+	'enableAjaxValidation'=>true,
+	//'htmlOptions' => array('enctype' => 'multipart/form-data')
+)); ?>
+
+	<div class="dialog-content">
+		<?php echo Phrase::trans(172,0);?>
+		
+	</div>
+	<div class="dialog-submit">
+		<?php echo CHtml::submitButton(Phrase::trans(173,0), array('onclick' => 'setEnableSave()')); ?>
+		<?php echo CHtml::button(Phrase::trans(174,0), array('id'=>'closed')); ?>
+	</div>
+	
+<?php $this->endWidget(); ?>
